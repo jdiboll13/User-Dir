14712424588fdfc6c4f010acd4b5f40cb0472a92
Jeremiah Diboll <jdiboll@gmail.com>
2017-07-25 21:52:30 -0400

    I should have commited sooner

---
+++ app.js
@@ -0,0 +1,28 @@
+const data = require('data.js')
+const express = require('express')
+const path = require('path')
+const mustacheExpress = require('mustache-express')
+const app = express()
+
+app.use(express.static('public'))
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.listen(3000, () => {
+  console.log("Let's do this!")
+})
+
+app.get('/', (req, res) => {
+  res.render('', data)
+})
+app.get('/users/:username', (req, res) => {
+  const userData = {
+    username: req.params.username
+  }
+  function findUser(user) {
+    return user.username === userData.username
+  }
+  const oneUser = data.users.find(findUser)
+  res.render('users', oneUser)
+})
